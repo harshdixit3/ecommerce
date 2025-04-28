d89179ae8cc89f80c7e3672b0e2026e4fe9fc3cb
Harsh Dixit <143201960+harshdixit3@users.noreply.github.com>
2025-04-29 00:50:51 +0530

    Completed all the ui and function of cust om sign-in sign-up page

--- app/(website)/(auth)/layout.tsx
+++ app/(website)/(auth)/layout.tsx
@@ -2,12 +2,15 @@
 import React from "react";
 import { MoveLeft } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
   return (
     <div className="h-screen">
       <div className="absolute top-10  left-10 flex items-center gap-4 group">
         <Button
+          onClick={() => router.back()}
           variant="nostyle"
           className="text-h4 group-hover:text-primary-600 gap-8 items-center"
         >
--- providers/index.tsx
+++ providers/index.tsx
@@ -4,7 +4,24 @@ import { ClerkProvider } from "@clerk/nextjs";
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
-      <ClerkProvider> {children}</ClerkProvider>
+      <ClerkProvider
+        //custom style
+        appearance={{
+          layout: {
+            socialButtonsPlacement: "bottom",
+            socialButtonsVariant: "blockButton",
+            logoImageUrl: "/assets/images/favicon.ico",
+          },
+        }}
+        afterSignOutUrl="/sign-in"
+        afterSignInUrl="/account/dashboard"
+        signInUrl={`${process.env.NEXT_PUBLIC_CLERK_SIGN_IN_URL}`}
+        signUpUrl={`${process.env.NEXT_PUBLIC_CLERK_SIGN_UP_URL}`}
+        signInFallbackRedirectUrl="/"
+        signUpFallbackRedirectUrl="/"
+      >
+        {children}
+      </ClerkProvider>
     </div>
   );
 };
